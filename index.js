a68ea6e723b16ee943dd4cd699299f5190b4c8da
Ariel Maniago <admaniago01@gmail.com>
2024-05-22 21:00:54 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+(async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+app.use("/api/posts", require("./routes/posts"));
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ routes/posts.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+const Post = require("../model/Posts");
+
+//create a posts data
+router.post("/create_post", async (req, res) => {
+  try {
+    const savedPost = await Post.create(req.body);
+    res.status(200).json(savedPost);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//get all posts
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find().lean();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//get single posts by slug
+router.get("/:slug", async (req, res) => {
+  try {
+    const post = await Post.findOne({ slug: req.params.slug }).lean();
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//update posts
+router.put("/:slug", async (req, res) => {
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(
+      { slug: req.params.slug },
+      { $set: req.body },
+      { new: true, upsert: true }
+    );
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+
+module.exports = router
