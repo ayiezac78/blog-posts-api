f6f2066f588241082b54ffa31dc121f61b74f253
Ariel Maniago <admaniago01@gmail.com>
2024-05-22 22:53:13 +0800

    added column in post model

--- model/Posts.js
+++ model/Posts.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema({
   slug: String,
   title: String,
+  sypnosis: String
 }, { timestamps: true });
 
 module.exports = mongoose.model("Posts", PostSchema);
